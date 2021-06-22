c5412ba06b9d44eb6ba6cbea48e8dbc1b7855a91
kmi0817 <kmi0817@naver.com>
2021-06-22 23:30:21 +0900

    searching movie title, then description will be written

--- scripts.js
+++ scripts.js
@@ -10,32 +10,55 @@ app.appendChild(logo)
 app.appendChild(container)
 
 /* my code  start */
-const question = document.createElement('p')
-question.textContent = "movie title?"
-app.appendChild(question)
+// const question = document.createElement('p')
+// question.textContent = "movie title?"
+// app.appendChild(question)
 
 const input = document.createElement('input')
 input.setAttribute('type', 'text')
+input.setAttribute('value', 'movie title?')
 app.appendChild(input)
 
 function get_description(title) {
-  data =
+  data = JSON.parse(request.response)
+
+  data.forEach((m) => {
+    if (m.title === title) {
+      const card = document.createElement('div')
+      card.setAttribute('class', 'card')
+      var description = m.description.substring(0, 300)
+      description = `${description}...`
+      card.textContent = description
+
+      const h1 = document.createElement('h1')
+      h1.textContent = m.title
+
+      container.appendChild(h1)
+      container.appendChild(card)
+    }
+  })
 }
+
 const btn = document.createElement('input')
 btn.setAttribute('type', 'submit')
-btn.setAttribute('value', '입력')
-btn.addEventListener('click', function() {
-  var input_title = input.value
-  alert(input_title)
-})
+btn.setAttribute('value', 'submit')
+btn.setAttribute('onclick', 'get_description(input.value)')
 app.appendChild(btn)
+
+const btn2 = document.createElement('input')
+btn2.setAttribute('type', 'button')
+btn2.setAttribute('value', 'reload')
+btn2.setAttribute('onclick', 'location.reload()')
+app.appendChild(btn2)
+
+/* my code ends */
 
 var request = new XMLHttpRequest()
 
 request.open("GET", "https://ghibliapi.herokuapp.com/films", true)
 
 request.onload = function() {
-  var data = JSON.parse(this.response)
+  var data = JSON.parse(this.response) // this == request.
 
   if (request.status >= 200 && request.status <= 400) {
     for (var i = 0; i < data.length; i++) {
